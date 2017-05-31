62804f9148241ba4b4434678f4b5cbca744081cc
omeaafjes <bvanael@telenet.be>
2017-05-31 21:10:52 +0200

    Add files via upload

---
+++ RandomPermutation.tcl
@@ -0,0 +1,17 @@
+proc Perm { input } {
+ set length [ llength $input ]
+ set worklist $input
+ for { set i 0} { $i < $length} { incr i } {
+ set m [ expr $length - $i ]
+ set randv [ expr $m * rand() ]
+ set newindex [ expr int($randv) ]
+ set permu($i) [ lindex $worklist $newindex ]
+ set worklist [lreplace $worklist $newindex $newindex]
+ # puts "$permu($i)"
+ }
+ # puts "isarray: [array exists permu]"
+ return [array get permu]
+}
+ 
+
+ 
